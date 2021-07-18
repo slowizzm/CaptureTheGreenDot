45468573d64c2ac2ccacf65af4ff34d661ae348f
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-17 23:21:36 -0500

    cleanup unused statements, add UpdateFlagPos

--- Assets/Scripts/FlagManager.cs
+++ Assets/Scripts/FlagManager.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlagManager : MonoBehaviour {
-
-    public GameObject flagPrefab;
+public class FlagManager : MonoBehaviour
+{
+    public Flag flag;
+    public PlayerController player;
+    [HideInInspector] public bool bIsHeld = false;
+    public static FlagManager Instance { get; private set; }
     GameObject[] flagStart;
 
-    public void SpawnFlag() {
 
-        // find all flag starts
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+    void Start()
+    {
+        SetFlagStartPos();
+    }
+    private void Update()
+    {
+        if (bIsHeld)
+        {
+            UpdateFlagPos(new Vector3(player.transform.position.x, 5, player.transform.position.z));
+        }
+    }
+
+    public void SetFlagStartPos()
+    {
+
+        // get array of flag starts
         flagStart = GameObject.FindGameObjectsWithTag("flagStart");
 
-        // get random flag start and set spawn pos
+        // get random flag start and set pos
         int newIndex = Random.Range(0, flagStart.Length);
-        Vector3 spawnPosition = flagStart[newIndex].transform.position;
+        Vector3 startPosition = flagStart[newIndex].transform.position;
 
-        // spawn flag
-        GameObject flag = Instantiate(flagPrefab, spawnPosition, Quaternion.identity);
+        // set flag pos
+        UpdateFlagPos(startPosition);
+    }
+
+    public void UpdateFlagPos(Vector3 pos)
+    {
+        flag.transform.position = new Vector3(pos.x, pos.y, pos.z);
     }
 }
