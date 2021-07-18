efec705181dbc3ad02edff2108aad021d3c9fbd0
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-17 23:19:55 -0500

    cleanup

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -1,14 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flag : MonoBehaviour {
-
-	void Start () {
-		
-	}
-	
-	void Update () {
+public class Flag : MonoBehaviour
+{
+    void Update()
+    {
         // raise flag off ground
         transform.Rotate(0, 5, 0, Space.World);
     }
