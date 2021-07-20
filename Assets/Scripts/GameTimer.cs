9b0cb156beb5fe59ec9696327f26521786a4ce33
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-20 12:19:13 -0500

    add script

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+    public static GameTimer Instance { get; private set; }
+    public float timerLength = 10f;
+    public HUDManager hudManager;
+
+    DateTime endTime;
+    TimeSpan remainingTime;
+
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        Time.timeScale = 1.0f;
+    }
+
+    private void Start()
+    {
+        endTime = DateTime.Now.AddSeconds(timerLength);
+    }
+
+    private void LateUpdate()
+    {
+        remainingTime = endTime - DateTime.Now;
+
+        if (remainingTime.Seconds >= 0)
+        {
+            string displayTime = String.Format(":{1:00}", remainingTime.Minutes, remainingTime.Seconds);
+            hudManager.tmp_timer.text = displayTime;
+        }
+    }
+}
+
