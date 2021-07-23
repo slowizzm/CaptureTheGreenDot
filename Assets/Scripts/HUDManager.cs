277b234e3a08471d92189f93b7f7d3b1689aeb18
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-23 15:17:20 -0500

    cleanup

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -1,66 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI tmp_lives;
-    [SerializeField] private TextMeshProUGUI tmp_level;
-    [SerializeField] private TextMeshProUGUI tmp_flagIsHeld;
-    public TextMeshProUGUI tmp_timer;
+  [SerializeField] private TextMeshProUGUI tmp_lives;
+  [SerializeField] private TextMeshProUGUI tmp_level;
+  [SerializeField] private TextMeshProUGUI tmp_flagIsHeld;
+  public TextMeshProUGUI tmp_timer;
 
-    private void OnEnable()
-    {
-        PlayerController.DispatchPlayerHasFlagEvent += ShowFlagIndicator;
-        PlayerController.DispatchPlayerDroppedFlagEvent += HideFlagIndicator;
-        PlayerController.DispatchPlayerDeadEvent += UpdateLivesDisplay;
-    }
-    private void OnDestroy()
-    {
-        PlayerController.DispatchPlayerHasFlagEvent -= ShowFlagIndicator;
-        PlayerController.DispatchPlayerDroppedFlagEvent -= HideFlagIndicator;
-        PlayerController.DispatchPlayerDeadEvent -= UpdateLivesDisplay;
-    }
+  // register events
+  private void OnEnable()
+  {
+    PlayerController.DispatchPlayerHasFlagEvent += ShowFlagIndicator;
+    PlayerController.DispatchPlayerDroppedFlagEvent += HideFlagIndicator;
+    PlayerController.DispatchPlayerDeadEvent += UpdateLivesDisplay;
+  }
+  // deregister events
+  private void OnDestroy()
+  {
+    PlayerController.DispatchPlayerHasFlagEvent -= ShowFlagIndicator;
+    PlayerController.DispatchPlayerDroppedFlagEvent -= HideFlagIndicator;
+    PlayerController.DispatchPlayerDeadEvent -= UpdateLivesDisplay;
+  }
 
-    void Start()
+  void Start()
+  {
+    // lives = GameManager.lives;
+    tmp_lives.text = "Lives: " + GameManager.lives.ToString();
+    tmp_level.text = "Score: " + GameManager.level.ToString();
+    UpdateLivesDisplay();
+    UpdateScoreDisplay();
+  }
+  private void Update()
+  {
+    tmp_timer.text = GameManager.timer;
+    // Debug.Log(GameManager.timer);
+  }
+  //set lives text
+  // not a fan of the overloading, refactor later
+  public void UpdateLivesDisplay()
+  {
+    if (GameManager.lives > 0)
     {
-        // lives = GameManager.lives;
-        tmp_lives.text = "Lives: " + GameManager.lives.ToString();
-        tmp_level.text = "Score: " + GameManager.level.ToString();
-        UpdateLivesDisplay();
-        UpdateScoreDisplay();
+      tmp_lives.text = $"Lives: {GameManager.lives.ToString()}";
     }
-    private void Update()
-    {
-        tmp_timer.text = GameManager.timer;
-        // Debug.Log(GameManager.timer);
-    }
-    //set lives text
-    // not a fan of the overloading, refactor later
-    public void UpdateLivesDisplay()
-    {
-        if (GameManager.lives > 0)
-        {
-            tmp_lives.text = $"Lives: {GameManager.lives.ToString()}";
-        }
-    }
-    public void UpdateLivesDisplay(PlayerController e)
-    {
-        tmp_lives.text = $"Lives: 0";
-    }
-    public void UpdateScoreDisplay()
-    {
-        tmp_level.text = $"Level: {GameManager.level}";
-    }
+  }
+  public void UpdateLivesDisplay(PlayerController e)
+  {
+    tmp_lives.text = $"Lives: 0";
+  }
+  public void UpdateScoreDisplay()
+  {
+    tmp_level.text = $"Level: {GameManager.level}";
+  }
 
-    public void ShowFlagIndicator(PlayerController e)
-    {
-        tmp_flagIsHeld.text = "Dot Captured";
-    }
-    public void HideFlagIndicator(PlayerController e)
-    {
-        tmp_flagIsHeld.text = " ";
-    }
+  public void ShowFlagIndicator(PlayerController e)
+  {
+    tmp_flagIsHeld.text = "Dot Captured";
+  }
+  public void HideFlagIndicator(PlayerController e)
+  {
+    tmp_flagIsHeld.text = " ";
+  }
 }
