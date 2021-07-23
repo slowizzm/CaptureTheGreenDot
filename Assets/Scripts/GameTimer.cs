54a8ccf692b2431788530682ded860398e828b76
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-23 15:17:01 -0500

    add comments

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -1,95 +1,95 @@
 ﻿using UnityEngine;
 using System;
-using System.Collections;
 
 public class GameTimer : MonoBehaviour
 {
-    public static GameTimer Instance { get; private set; }
-    public float timerLength = 10f;
-    // public HUDManager hudManager;
-    public float elapsedTime;
+  public static GameTimer Instance { get; private set; }
+  public float timerLength = 10f;
+  // public HUDManager hudManager;
+  public float elapsedTime;
 
-    DateTime endTime;
-    TimeSpan remainingTime;
-    bool bTimeIsOut = false;
-    bool bCanSendEvent = true;
+  DateTime endTime;
+  TimeSpan remainingTime;
+  bool bTimeIsOut = false;
+  bool bCanSendEvent = true;
 
-    public static event Action<GameTimer> DispatchTimeIsOutEvent = delegate { };
-    private TimeSpan timePlaying;
-    private bool bIsTimerGoing = false;
-    public static bool bStartTimer = false;
+  public static event Action<GameTimer> DispatchTimeIsOutEvent = delegate { };
+  private TimeSpan timePlaying;
+  private bool bIsTimerGoing = false;
+  public static bool bStartTimer = false;
 
-    Coroutine co;
+  Coroutine co;
 
+  // register events
+  private void Awake()
+  {
+    if (Instance == null) Instance = this;
+  }
+  // deregister events
+  private void OnEnable()
+  {
+    GameManager.DispatchStartGameEvent += StartTimer;
+    PlayerController.DispatchPlayerDeadEvent += StopTimer;
+  }
 
-    private void Awake()
+  private void InitTimer()
+  {
+    elapsedTime = GameManager.time;
+    endTime = DateTime.Now.AddSeconds(timerLength);
+
+    bIsTimerGoing = true;
+    bCanSendEvent = true;
+    bTimeIsOut = false;
+  }
+
+  private void StartTimer<T>(T e)
+  {
+    InitTimer();
+  }
+  public void StartTimer()
+  {
+    InitTimer();
+  }
+  private void StopTimer<T>(T e)
+  {
+    bIsTimerGoing = false;
+  }
+
+  private void LateUpdate()
+  {
+    if (!bIsTimerGoing) return;
+
+    GetElapsedTime();
+    CountdownTime();
+
+  }
+
+  private void CountdownTime()
+  {
+    remainingTime = endTime - DateTime.Now;
+
+    if (remainingTime.Seconds >= 0)
     {
-        if (Instance == null) Instance = this;
+      string displayTime = String.Format(":{1:00}", remainingTime.Minutes, remainingTime.Seconds);
+      GameManager.timer = displayTime;
     }
-    private void OnEnable()
+    else
     {
-        GameManager.DispatchStartGameEvent += StartTimer;
-        PlayerController.DispatchPlayerDeadEvent += StopTimer;
-    }
-
-    private void InitTimer()
-    {
-        elapsedTime = GameManager.time;
-        endTime = DateTime.Now.AddSeconds(timerLength);
-
-        bIsTimerGoing = true;
-        bCanSendEvent = true;
-        bTimeIsOut = false;
-    }
-
-    private void StartTimer<T>(T e)
-    {
-        InitTimer();
-    }
-    public void StartTimer()
-    {
-        InitTimer();
-    }
-    private void StopTimer<T>(T e)
-    {
-        bIsTimerGoing = false;
-    }
-
-    private void LateUpdate()
-    {
-        if (!bIsTimerGoing) return;
-
-        GetElapsedTime();
-        CountdownTime();
-
-    }
-
-    private void CountdownTime()
-    {
-        remainingTime = endTime - DateTime.Now;
-
-        if (remainingTime.Seconds >= 0)
-        {
-            string displayTime = String.Format(":{1:00}", remainingTime.Minutes, remainingTime.Seconds);
-            GameManager.timer = displayTime;
-        }
-        else
-        {
-            bTimeIsOut = true;
-        }
-
-        if (bTimeIsOut && bCanSendEvent)
-        {
-            // Debug.Log("out of time");
-            bCanSendEvent = false;
-            DispatchTimeIsOutEvent(this);
-        }
+      bTimeIsOut = true;
     }
 
-    private void GetElapsedTime()
+    if (bTimeIsOut && bCanSendEvent)
     {
-        elapsedTime += Time.deltaTime;
-        GameManager.time = elapsedTime;
+      // Debug.Log("out of time");
+      bCanSendEvent = false;
+      DispatchTimeIsOutEvent(this);
     }
+  }
+
+  private void GetElapsedTime()
+  {
+    elapsedTime += Time.deltaTime;
+    GameManager.time = elapsedTime;
+  }
 }
 
