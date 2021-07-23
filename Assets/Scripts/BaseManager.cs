194e0b497d9754e41b87f5db63e6d9e1a4a0a5df
Raymond G McCord <raymondgmccord@gmail.com>
2021-07-23 11:30:41 -0500

    tweak

--- Assets/Scripts/BaseManager.cs
+++ Assets/Scripts/BaseManager.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class BaseManager : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        PlayerController.DispatchPlayerHasFlagEvent += UpdateBaseColorGreen;
-        PlayerController.DispatchPlayerDroppedFlagEvent += UpdateBaseColorRed;
-    }
-    private void OnDestroy()
-    {
-        PlayerController.DispatchPlayerHasFlagEvent -= UpdateBaseColorGreen;
-        PlayerController.DispatchPlayerDroppedFlagEvent -= UpdateBaseColorRed;
-    }
+  private void OnEnable()
+  {
+    PlayerController.DispatchPlayerHasFlagEvent += UpdateBaseColorGreen;
+    PlayerController.DispatchPlayerDroppedFlagEvent += UpdateBaseColorRed;
+  }
+  private void OnDestroy()
+  {
+    PlayerController.DispatchPlayerHasFlagEvent -= UpdateBaseColorGreen;
+    PlayerController.DispatchPlayerDroppedFlagEvent -= UpdateBaseColorRed;
+  }
 
-    private void UpdateBaseColorRed<T>(T e)
-    {
-        // Debug.Log("base: red");
-        gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0.3f, 0f, 0.3f, 0.2f));
-    }
-    private void UpdateBaseColorGreen<T>(T e)
-    {
-        // Debug.Log("base: green");
-        gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0f, 1f, 0.8f, 0.7f));
-    }
+  private void UpdateBaseColorRed<T>(T e)
+  {
+    // Debug.Log("base: red");
+    gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0.3f, 0f, 0.3f, 0.2f));
+  }
+  private void UpdateBaseColorGreen<T>(T e)
+  {
+    // Debug.Log("base: green");
+    gameObject.GetComponent<Renderer>().material.SetColor("_Color", new Color(0f, 1f, 0.8f, 0.7f));
+  }
 }
